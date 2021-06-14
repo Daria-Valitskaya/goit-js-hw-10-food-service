bd30a306f45cf5d9772eaab6daae116b12839783
Daria-Valitskaya <75114227+Daria-Valitskaya@users.noreply.github.com>
2021-06-15 00:59:46 +0300

    добавила разметку

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
+import menuCard from './templates/menu-cards.hbs';
+import menu from './menu.json';
 import './sass/main.scss';
+
+const menuContainer = document.querySelector('.js-menu');
+const changerTheme = document.querySelector('#theme-switch-toggle')
+const menuMarkup = createMenuItem (menu);
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+
+menuContainer.insertAdjacentHTML('beforeend', menuMarkup);
+changerTheme.addEventListener('change', switcherTheme);
+
+
+function createMenuItem(menu){
+    return menu.map(menuCard).join('')
+};
+
+
+function switcherTheme(){};
+
