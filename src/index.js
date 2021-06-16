f096757f03d7e7e76b59e6c1551ac74904f7a714
Daria-Valitskaya <75114227+Daria-Valitskaya@users.noreply.github.com>
2021-06-16 23:35:34 +0300

    рабочая дз 10

--- src/index.js
+++ src/index.js
@@ -1,24 +1,14 @@
 import menuCard from './templates/menu-cards.hbs';
 import menu from './menu.json';
+import theme from './theme.js';
 import './sass/main.scss';
 
 const menuContainer = document.querySelector('.js-menu');
-const changerTheme = document.querySelector('#theme-switch-toggle')
-const menuMarkup = createMenuItem (menu);
-const Theme = {
-  LIGHT: 'light-theme',
-  DARK: 'dark-theme',
-};
 
+const menuMarkup = createMenuItem(menu);
 
 menuContainer.insertAdjacentHTML('beforeend', menuMarkup);
-changerTheme.addEventListener('change', switcherTheme);
 
-
-function createMenuItem(menu){
-    return menu.map(menuCard).join('')
-};
-
-
-function switcherTheme(){};
-
+function createMenuItem(menu) {
+  return menu.map(menuCard).join('');
+}
---
+++ src/theme.js
@@ -0,0 +1,25 @@
+const body = document.querySelector('body');
+const changerTheme = document.querySelector('#theme-switch-toggle');
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+changerTheme.addEventListener('change', switcherTheme);
+
+function switcherTheme() {
+  if (changerTheme.checked) {
+    body.classList.add(Theme.DARK);
+    body.classList.remove(Theme.LIGHT);
+    localStorage.setItem('theme', Theme.DARK);
+  } else {
+    body.classList.add(Theme.LIGHT);
+    body.classList.remove(Theme.DARK);
+    localStorage.setItem('theme', Theme.LIGHT);
+  }
+}
+
+if (localStorage.getItem('theme') === Theme.DARK) {
+  changerTheme.checked = true;
+  body.classList.add(Theme.DARK);
+}
